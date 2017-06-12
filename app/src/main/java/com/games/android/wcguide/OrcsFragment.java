f47b20da315699fb7e1c5320ef78ff298ea40561
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2017-06-12 23:28:22 +0300

    Initial commit.

---
+++ app/src/main/java/com/games/android/wcguide/OrcsFragment.java
@@ -0,0 +1,9 @@
+package com.games.android.wcguide;
+
+public class OrcsFragment extends RaceFragment {
+
+    @Override
+    protected String getRaceTitle() {
+        return getString(R.string.orcs);
+    }
+}
